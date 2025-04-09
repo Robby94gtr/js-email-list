0c9a53a6c389ecd5bc615a6191e754b2ae7a8d73
Robby94gtr <robertogramazio94@gmail.com>
2025-04-09 17:53:43 +0200

    [UPD] Linked endpoint's API

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 // Recupero gli elementi del DOM che mi occorrono
 const listEmails = document.getElementById('emailsList');
+
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
