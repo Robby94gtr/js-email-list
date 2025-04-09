a5a1303327e8408bcffd97a0c20295cb16f7a8a6
Robby94gtr <robertogramazio94@gmail.com>
2025-04-09 17:59:07 +0200

    END

--- js/script.js
+++ js/script.js
@@ -2,3 +2,14 @@
 const listEmails = document.getElementById('emailsList');
 
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+for(let i=0; i<10; i++){
+    // Questo ciclo for ripete 10 volte una richiesta all’API.
+    // Sto dicendo:"Fammi 10 richieste e per ognuna, prendi l'email e mettila nella lista."
+    axios.get(endpoint).then((resp) => {
+        // Con Axios faccio una richiesta HTTP GET verso l’API.
+        // Quando il server risponde, entro dentro la funzione .then().
+        const li = `<li>${resp.data.response}</li>`
+        listEmails.innerHTML += li
+    })
+}
